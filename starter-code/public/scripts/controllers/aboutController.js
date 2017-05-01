b09d5cbf099c983ef9b7e91e214e3d56be7b35fd
pmendoza72 <pablo_mendoza@hotmail.com>
2017-05-01 02:58:32 -0700

    added functions and routes. installed page

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,7 +4,9 @@
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
-
+  aboutController.reveal = () => {
+    $('#articles').hide();
+    $('#about').show();
+  }
   module.aboutController = aboutController;
 })(window);
